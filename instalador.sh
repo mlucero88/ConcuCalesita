7de101757e48cab998d37cc70088f084467f0f53
Martin Lucero <luceromartinandres89630@gmail.com>
2014-10-15 04:34:30 -0300

    Instalador y manual

--- instalador.sh
+++ instalador.sh
@@ -2,11 +2,6 @@
 
 # $1 Carpeta a instalar
 
-RUTA_RAIZ="$1/Concucalesita"
-let FORCE=0
-let CLEAN_CFG=0
-let CLEAN_GENERATED=0
-
 if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ $# -ne 1 ] ; then
 	echo "> Uso: instalador <ruta>"
 	echo "> ruta: directorio donde se instalará el programa"
@@ -17,6 +12,8 @@ if [ ! -d "$1" ]; then
 	echo "error: no existe el directorio $1"
 	exit 2
 fi
+
+RUTA_RAIZ="$(echo "$1" | sed 's/\/$//')/Concucalesita"
 
 mkdir -p "${RUTA_RAIZ}"
 mkdir -p "${RUTA_RAIZ}/bin"
@@ -32,5 +29,74 @@ echo "<precio_boleto> 7.50" > "${RUTA_RAIZ}/conf/config"
 echo "<capacidad_calesita> 30" >> "${RUTA_RAIZ}/conf/config"
 echo "<duracion_vuelta_calesita> 15" >> "${RUTA_RAIZ}/conf/config"
 echo "<ninios_a_generar> 200" >> "${RUTA_RAIZ}/conf/config"
+
+PATH_PROYECTOS="$(dirname "$0")"
+
+#Compilacion IPC
+mkdir -p "${PATH_PROYECTOS}/IPC/obj"
+
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Common/ByteStream.cpp" -o "${PATH_PROYECTOS}/IPC/obj/ByteStream.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Lock/LockException.cpp" -o "${PATH_PROYECTOS}/IPC/obj/LockException.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Lock/Lock.cpp" -o "${PATH_PROYECTOS}/IPC/obj/Lock.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Pipe/Deserializador.cpp" -o "${PATH_PROYECTOS}/IPC/obj/Deserializador.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Pipe/Fifo.cpp" -o "${PATH_PROYECTOS}/IPC/obj/Fifo.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Pipe/FifoEscritura.cpp" -o "${PATH_PROYECTOS}/IPC/obj/FifoEscritura.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Pipe/FifoLectura.cpp" -o "${PATH_PROYECTOS}/IPC/obj/FifoLectura.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Pipe/ITraductor.cpp" -o "${PATH_PROYECTOS}/IPC/obj/ITraductor.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Pipe/Serializador.cpp" -o "${PATH_PROYECTOS}/IPC/obj/Serializador.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Seniales/SignalHandler.cpp" -o "${PATH_PROYECTOS}/IPC/obj/SignalHandler.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Shared_Memory/SharedMemoryException.cpp" -o "${PATH_PROYECTOS}/IPC/obj/SharedMemoryException.o"
+g++ -O2 -c "${PATH_PROYECTOS}/IPC/src/Shared_Memory/SharedMemoryBlock.cpp" -o "${PATH_PROYECTOS}/IPC/obj/SharedMemoryBlock.o"
+
+ar -r "${PATH_PROYECTOS}/IPC/libIPC.a" "${PATH_PROYECTOS}/IPC/obj/ByteStream.o" "${PATH_PROYECTOS}/IPC/obj/LockException.o" "${PATH_PROYECTOS}/IPC/obj/Lock.o" "${PATH_PROYECTOS}/IPC/obj/Deserializador.o" "${PATH_PROYECTOS}/IPC/obj/Fifo.o" "${PATH_PROYECTOS}/IPC/obj/FifoEscritura.o" "${PATH_PROYECTOS}/IPC/obj/FifoLectura.o" "${PATH_PROYECTOS}/IPC/obj/ITraductor.o" "${PATH_PROYECTOS}/IPC/obj/Serializador.o" "${PATH_PROYECTOS}/IPC/obj/SignalHandler.o" "${PATH_PROYECTOS}/IPC/obj/SharedMemoryException.o" "${PATH_PROYECTOS}/IPC/obj/SharedMemoryBlock.o"
+
+#Compilacion Comun
+mkdir -p "${PATH_PROYECTOS}/Comun/obj"
+
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" "${PATH_PROYECTOS}/Comun/src/Logger.cpp" -o "${PATH_PROYECTOS}/Comun/obj/Logger.o"
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" "${PATH_PROYECTOS}/Comun/src/Ninio.cpp" -o "${PATH_PROYECTOS}/Comun/obj/Ninio.o"
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" "${PATH_PROYECTOS}/Comun/src/TraductorNinio.cpp" -o "${PATH_PROYECTOS}/Comun/obj/TraductorNinio.o"
+
+ar -r "${PATH_PROYECTOS}/Comun/libComun.a" "${PATH_PROYECTOS}/Comun/obj/Logger.o" "${PATH_PROYECTOS}/Comun/obj/Ninio.o" "${PATH_PROYECTOS}/Comun/obj/TraductorNinio.o"
+
+#Compilacion Boleteria
+mkdir -p "${PATH_PROYECTOS}/ProcesoBoleteria/obj"
+
+g++ -O2 -c -Wno-unused-result -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoBoleteria/src/Boleteria.cpp" -o "${PATH_PROYECTOS}/ProcesoBoleteria/obj/Boleteria.o"
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoBoleteria/src/Caja.cpp" -o "${PATH_PROYECTOS}/ProcesoBoleteria/obj/Caja.o"
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoBoleteria/src/boleteria.cpp" -o "${PATH_PROYECTOS}/ProcesoBoleteria/obj/boleteria.o"
+
+g++ -L"${PATH_PROYECTOS}/IPC" -L"${PATH_PROYECTOS}/Comun" -o "${RUTA_RAIZ}/bin/boleteria" "${PATH_PROYECTOS}/ProcesoBoleteria/obj/Boleteria.o" "${PATH_PROYECTOS}/ProcesoBoleteria/obj/Caja.o" "${PATH_PROYECTOS}/ProcesoBoleteria/obj/boleteria.o" -lComun -lIPC -ljsoncpp
+
+#Compilacion Calesita
+mkdir -p "${PATH_PROYECTOS}/ProcesoCalesita/obj"
+
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoCalesita/src/Calesita.cpp" -o "${PATH_PROYECTOS}/ProcesoCalesita/obj/Calesita.o"
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoCalesita/src/calesita.cpp" -o "${PATH_PROYECTOS}/ProcesoCalesita/obj/calesita.o"
+
+g++ -L"${PATH_PROYECTOS}/IPC" -L"${PATH_PROYECTOS}/Comun" -o "${RUTA_RAIZ}/bin/calesita"  "${PATH_PROYECTOS}/ProcesoCalesita/obj/Calesita.o" "${PATH_PROYECTOS}/ProcesoCalesita/obj/calesita.o" -lComun -ljsoncpp -lIPC
+
+#Compilacion Generador
+mkdir -p "${PATH_PROYECTOS}/ProcesoGenerador/obj"
+
+g++ -O2 -c -Wno-unused-result -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoGenerador/src/generador.cpp" -o "${PATH_PROYECTOS}/ProcesoGenerador/obj/generador.o"
+
+g++ -L"${PATH_PROYECTOS}/IPC" -L"${PATH_PROYECTOS}/Comun" -o "${RUTA_RAIZ}/bin/generador"  "${PATH_PROYECTOS}/ProcesoGenerador/obj/generador.o" -lComun -ljsoncpp -lIPC
+
+#Compilacion Administrador
+mkdir -p "${PATH_PROYECTOS}/ProcesoAdministrador/obj"
+
+g++ -O2 -c -Wno-unused-result -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoAdministrador/src/Administrador.cpp" -o "${PATH_PROYECTOS}/ProcesoAdministrador/obj/Administrador.o"
+g++ -O2 -c -I"${PATH_PROYECTOS}/IPC/src" -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoAdministrador/src/admin.cpp" -o "${PATH_PROYECTOS}/ProcesoAdministrador/obj/admin.o"
+
+g++ -L"${PATH_PROYECTOS}/IPC" -L"${PATH_PROYECTOS}/Comun" -o "${RUTA_RAIZ}/bin/administrador"  "${PATH_PROYECTOS}/ProcesoAdministrador/obj/Administrador.o" "${PATH_PROYECTOS}/ProcesoAdministrador/obj/admin.o" -lComun -lIPC
+
+#Compilacion Lanzador
+mkdir -p "${PATH_PROYECTOS}/ProcesoLanzador/obj"
+
+g++ -O2 -c -Wno-unused-result -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoLanzador/src/Configuracion.cpp" -o "${PATH_PROYECTOS}/ProcesoLanzador/obj/Configuracion.o"
+g++ -O2 -c -Wno-unused-result -I"${PATH_PROYECTOS}/Comun/src" "${PATH_PROYECTOS}/ProcesoLanzador/src/lanzador.cpp" -o "${PATH_PROYECTOS}/ProcesoLanzador/obj/lanzador.o"
+ 
+g++ -L"${PATH_PROYECTOS}/Comun" -o "${RUTA_RAIZ}/bin/lanzador" "${PATH_PROYECTOS}/ProcesoLanzador/obj/Configuracion.o" "${PATH_PROYECTOS}/ProcesoLanzador/obj/lanzador.o" -lComun
 
 echo "> Instalación exitosa"
